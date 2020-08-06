81bf39c53e98413ec85d15c79283ef4fdbcdf3d9
LongQue <1220756253@qq.com>
2020-08-06 23:14:15 +0800

    爬取相亲网站城市

--- crawler/main.go
+++ crawler/main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"awesomeProject1/crawler/engine"
+	"awesomeProject1/crawler/zhenai/parser"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:8080/mock/www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code", resp.StatusCode)
-		return
-	}
-
-	all, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	printCityList(all)
-}
-func printCityList(contents []byte) {
-	//<a href="http://localhost:8080/mock/www.zhenai.com/zhenghun/zunyi"class = "">遵义</a>
-	re := regexp.MustCompile(`<a href="http://localhost:8080/mock/www.zhenai.com/zhenghun/[0-9a-z]+"[^>]*>[^<]+</a>`)
-	matchs := re.FindAll(contents, -1)
-	for _,m:=range matchs{
-		fmt.Printf("%s/n",m)
-	}
-	fmt.Printf("Matches founc: %d\n",len(matchs))
+	engine.Run(engine.Request{
+		Url:        "http://localhost:8080/mock/www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module awesomeProject1
 
 go 1.13
 
-require golang.org/x/text v0.3.3
+require (
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381
+	golang.org/x/text v0.3.3
+)
